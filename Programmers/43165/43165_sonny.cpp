cc6fa65552c247d0c78810b3b2130626c9bbae8d
sonny <s.junhyuck@gmail.com>
2023-10-17 16:03:37 +0900

    43165 : target number

---
+++ Programmers/43165/43165_sonny.cpp
@@ -0,0 +1,102 @@
+// ========== STACK =========
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+struct node
+{
+    int num;
+    int count;
+
+    node(int in_num, int in_count) : num(in_num), count(in_count) { }
+};
+
+bool compare_stack_target(vector<int> stack, int target)
+{
+    int sum = 0;
+    for(int& item : stack)
+    {
+        sum += item;
+    }
+
+    return sum == target;
+}
+
+// DFS로 구현
+void get_target_number(vector<int> numbers, int target, int sum, int index, int& out_answer)
+{
+}
+
+int solution(vector<int> numbers, int target) 
+{
+    int answer = 0;
+    vector<node> stack;
+    node positive_start_node = node(numbers[0], 0);
+    node negative_start_node = node(-numbers[0], 0);
+
+    stack.push_back(negative_start_node);
+    stack.push_back(positive_start_node);
+    
+    while(!stack.empty())
+    {
+        int num = stack.back().num;
+        int count = stack.back().count;
+        int level = count + 1;
+        stack.pop_back();
+
+        if(level >= numbers.size())
+        {
+            if(num == target)
+                answer++;
+
+            continue;
+        }
+
+        count++;
+
+        stack.push_back( {num - numbers[count], count} );
+        stack.push_back( {num + numbers[count], count} );
+    }
+
+    return answer;
+}
+
+// ========== RECULSIVE =========
+
+// #include <string>
+// #include <vector>
+
+// using namespace std;
+
+// // DFS로 구현
+// void get_target_number(vector<int> numbers, int target, int sum, int index, int& out_answer)
+// {
+//     // 모든 numbers를 소모했다면
+//     if(index == numbers.size())
+//     {
+//         // 모든 숫자를 소모했을 때, 결과값이 target과 같을때만 ++
+//         if(sum == target)
+//         {
+//             out_answer++;
+//         }
+        
+//         return;
+//     }
+    
+//     // +로 분기
+//     get_target_number(numbers, target, sum + numbers[index], index + 1, out_answer);
+    
+//     // -로 분기
+//     get_target_number(numbers, target, sum - numbers[index], index + 1, out_answer);
+// }
+
+// int solution(vector<int> numbers, int target) 
+// {
+//     int answer = 0;
+    
+//     get_target_number(numbers, target, 0, 0, answer);
+    
+//     return answer;
+// }
